7f69430a9f38859b0d065ec224dc934266b5f562
Eduardo <132284188+Dioque@users.noreply.github.com>
2026-06-04 13:58:14 -0300

    ti

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,4 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
+export const SITE_TITLE = 'BitBelow Game Studio';
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION =
+	'BitBelow é um estúdio independente focado em criar experiências únicas através de jogos de horror, exploração e narrativa.';
